8af47f121a7675539cc56bf81160b6f6c4f7c88a
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-03 15:53:53 +0530

    extract style and scripts from sub pages into the layout and set the path or assets and require express ejs layout

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+
+// using expressLayouts
+app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //use express router
 app.use("/", require("./routes"));
 
 // setup of view engine
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/assets"));
+
 app.listen(port, function (err) {
   if (err) {
     console.log("error", err);
