2bf3456b3ca449362c9328728dcd0e09e7a1f03f
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-03 14:03:51 +0530

    create a basic server and add middleware for views engine

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+//use express router
+app.use("/", require("./routes"));
+
+// setup of view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.listen(port, function (err) {
+  if (err) {
+    console.log("error", err);
+  } else {
+    console.log(`server is running on port ${port}`);
+  }
+});
