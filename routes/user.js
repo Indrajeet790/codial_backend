5a92b2a3fb6ab0ca844da4415ace258de9473cda
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-26 17:16:56 +0530

    create session routes for mannule auth

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ router.get("/profile", userController.profile);
 router.get("/sign-in", userController.signIn);
 router.get("/sign-up", userController.signup);
 router.post("/create", userController.create);
+router.post("/create-session", userController.createSession);
 
 module.exports = router;
