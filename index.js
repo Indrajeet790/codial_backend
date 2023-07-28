18d7c0abd7c5dd0c8c964b724da961aceb287c79
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-07-28 11:20:58 +0530

    require passport local and passport local-strategy and add express-session to create a session for user

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 const cookieParser = require("cookie-parser");
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
+// use session cookies
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport_local_strategy");
 
 //
 app.use(express.urlencoded());
@@ -18,13 +22,31 @@ app.use(expressLayouts);
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
-//use express router
-app.use("/", require("./routes"));
-
 // setup of view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + "/assets"));
+
+// passport middleware
+app.use(
+  session({
+    name: "codeial",
+    // todo change the secret before in production mode
+
+    secret: "something",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//use express router
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
