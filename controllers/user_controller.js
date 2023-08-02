c32fb60ebf537672297bd57464800e5a8f98266e
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-08-02 13:40:43 +0530

    design post Schema Model  and write html code for post sections

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -129,7 +129,7 @@ module.exports.create = async (req, res) => {
 
 /// sign in and create a session for the user
 module.exports.createSession = async (req, res) => {
-  return res.redirect("/");
+  return res.redirect("/users/profile");
 };
 
 // sign-out the session
