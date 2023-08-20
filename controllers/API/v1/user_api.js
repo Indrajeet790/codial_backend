4e32a09a8a08ed2c6284c454de91698fca5e65c3
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-08-20 09:40:37 +0530

    add comment on the code

--- controllers/API/v1/user_api.js
+++ controllers/API/v1/user_api.js
@@ -1,15 +1,20 @@
 const jwt = require("jsonwebtoken"); // Import the 'jsonwebtoken' library
 const User = require("../../../models/user");
 
+
+// Create a session by verifying user credentials and generating a JWT token
 module.exports.createSession = async (req, res) => {
   try {
+    // Find a user in the database based on the provided email
     const user = await User.findOne({ email: req.body.email }); // Add 'await' here
 
+    // Check if a user with the given email exists and if the provided password matches
     if (!user || user.password !== req.body.password) {
       return res.status(422).json({ message: "Invalid userName/password" });
     }
 
-    // Correct the 'expiresIn' property, and use 'jwt.sign' instead of 'JwtStrategy.sign'
+        // Generate a new JWT token with the user's data as the payload, a secret key "codeial",
+    // and set the token's expiration time to 1 day (24 hours)
     const token = jwt.sign(user.toJSON(), "codeial", { expiresIn: "1d" });
 
     return res.status(200).json({
