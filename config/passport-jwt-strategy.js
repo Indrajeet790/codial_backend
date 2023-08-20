571dabaac186861120a19e1844b1ed95b0d56985
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-08-20 09:44:26 +0530

    add comment on the code

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -3,20 +3,26 @@ const JwtStrategy=require("passport-jwt").Strategy;
 const ExtractJWT=require("passport-jwt").ExtractJwt;
 const User=require("../models/user");
 
+// Options for the JWT strategy configuration
 let opts={
-    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken,
-    secretOrKey :'codeial'
+    // Extract JWT token from the authorization header
+    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken, 
+    secretOrKey: 'codeial' // Secret key to decode the JWT token
 }
+// Configure Passport to use the JWT strategy
 passport.use(new JwtStrategy(opts, function(jwt_payload, done){
+      // Find a user in the database based on the ID extracted from the JWT payload
     User.findOne({id: jwt_payload._id}, function(err, user){
         if(err){
             console.log("error",err);
             return;
         }
         if(user){
+              // If a user is found, pass the user data to the next middleware
             return done(null,user)
         }else{
-            return done(null,false)
+              // If no user is found, deny access by passing 'false' to the next middleware
+              return done(null, false);
         }
 
     })
